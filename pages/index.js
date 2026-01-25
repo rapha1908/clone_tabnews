6dcdd8a5a74a2f7bab76c433d47e29a3431f8c0d
Raphael Da Silva <31869527+rapha1908@users.noreply.github.com>
2026-01-25 22:22:57 +0000

    Adicionando os pacotes e a pagina do produto

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home(){
+    return(
+        <h1>Só queria dizer que te amo</h1>
+    )
+}
+export default Home;
---
+++ pages/produtos/index.js
@@ -0,0 +1,6 @@
+function Produtos(){
+    return(
+        <h1>Produto 1</h1>
+    )
+}
+export default Produtos
